eb8db9b2cc58d06b2512a756c9848ef75ab820b4
Sebastian Girsza <sebag96@wp.pl>
2022-09-18 14:56:05 +0200

    Add .from.Json method to our model

--- lib/domain/models/weather_model.dart
+++ lib/domain/models/weather_model.dart
@@ -8,4 +8,9 @@ class WeatherModel {
   final double temperature;
   final String city;
   final String country;
+
+  WeatherModel.fromJson(Map<String, dynamic> json)
+      : temperature = json['current']['temp_c'] + 0.0,
+        city = json['location']['name'],
+        country = json['location']['country'];
 }
--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -8,18 +8,21 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    final responseData = await _weatherRemoteDataSource.getWeatherData(
+    final json = await _weatherRemoteDataSource.getWeatherData(
       city: city,
     );
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
-    final country = responseData['location']['country'] as String;
-    final name = responseData['location']['name'] as String;
-    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
 
-    return WeatherModel(city: name, temperature: temperature, country: country);
+    return WeatherModel.fromJson(json);
   }
 }
     // final response = await Dio().get<Map<String, dynamic>>(
     //     'http://api.weatherapi.com/v1/current.json?key=a76cacad2a29458abdc120139221809&q=$city&aqi=no');
+
+        // final country = responseData['location']['country'] as String;
+    // final name = responseData['location']['name'] as String;
+    // final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+
+    // return WeatherModel(city: name, temperature: temperature, country: country);
